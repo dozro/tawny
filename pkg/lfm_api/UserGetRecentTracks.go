95a8f8d4bbd27dfe0b9c7b882a5142bc3ebf9a32
Rye <git@itsrye.dev>
2025-11-24 23:55:58 +0100

    removed debug logging that contained api keys

--- pkg/lfm_api/UserGetRecentTracks.go
+++ pkg/lfm_api/UserGetRecentTracks.go
@@ -2,13 +2,10 @@ package lfm_api
 
 import (
 	"github.com/dozro/tawny/pkg/lfm_types"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func (User) GetRecentTracks(args UserGetArgsWithLimitPage) (*lfm_types.UserGetRecentTracks, error) {
 	apiUrl := pageLimitAK(baseUrl, "user.getRecentTracks", args.UserName, args.ApiKey, args.Limit, args.Page)
-	log.Debugf("apiUrl: %s", apiUrl)
 
 	data, err := fetchXML[lfm_types.WrappedUserGetRecentTracks](apiUrl)
 
