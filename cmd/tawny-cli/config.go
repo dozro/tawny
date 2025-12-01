a54c2c7e48c14dcc072cf0ae2540e347c3d9c0dd
Rose <git@itsrye.dev>
2025-12-01 15:08:23 +0100

    refactor: Rename fm-cli to tawny-cli (enh/refactoring)
    This commit renames the fm-cli tool to tawny-cli.
    This includes:
    
    - Renaming the directory from cmd/fm-cli to cmd/tawny-cli.
    - Renaming the config struct and variable names.
    - Adding HMAC secret and debug config options.
    - Updating the build.md documentation.
    
    Signed-off-by: Rose <git@itsrye.dev>

--- cmd/tawny-cli/config.go
+++ cmd/tawny-cli/config.go
@@ -7,30 +7,36 @@ import (
 	sc "codeberg.org/dozrye/golang_simpleconfig"
 )
 
-type tawnyCliConfig struct {
+type TawnyCliConfig struct {
 	ApiKey      string
 	ApiEndpoint url.URL
 	Username    string
+	HMACSecret  string
+	Debug       bool
 	Op          string
 }
 
 var configHandler sc.SimpleConfigHandler
 
-func Flagread() tawnyCliConfig {
+func Flagread() TawnyCliConfig {
 	apiKeyFlag := configHandler.GetStringOption(sc.ConfigEntry{Key: "apikey", Description: "LastFm API Key"})
 	endpointFlag := configHandler.GetStringOption(sc.ConfigEntry{Key: "endpoint", DefaultString: "tawny.itsrye.uk", Description: "Tawny API Endpoint"})
 	usernameFlag := configHandler.GetStringOption(sc.ConfigEntry{Key: "username", Description: "LastFm Username"})
-	opFlag := configHandler.GetStringOption(sc.ConfigEntry{Key: "op", DefaultString: "user_tracks_current", Description: "Tawny Operation"})
+	hmacSecret := configHandler.GetStringOption(sc.ConfigEntry{Key: "hmacsecret", Description: "Tawny HMAC Secret"})
+	debugoutput := configHandler.GetBooleanOption(sc.ConfigEntry{Key: "debug", Description: "Tawny Debug Flag", DefaultBool: false})
+	op := configHandler.GetStringOption(sc.ConfigEntry{Key: "op", Description: "Tawny Op"})
 	configHandler.ParseFlags()
 	flag.Parse()
-	return tawnyCliConfig{
+	return TawnyCliConfig{
 		ApiKey: *apiKeyFlag,
 		ApiEndpoint: url.URL{
 			Scheme: "https",
 			Host:   *endpointFlag,
 			Path:   "/api/v1",
 		},
-		Username: *usernameFlag,
-		Op:       *opFlag,
+		Username:   *usernameFlag,
+		HMACSecret: *hmacSecret,
+		Debug:      *debugoutput,
+		Op:         *op,
 	}
 }
